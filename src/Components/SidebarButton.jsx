291cb0d49e00baf3ea01587111922afdc9ef09b2
Karume-lab <danielkarume@outlook.com>
2024-03-21 18:14:27 +0300

    fix sidebar overflow on large screen

--- src/Components/SidebarButton.jsx
+++ src/Components/SidebarButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Tab({ icon, text }) {
   return (
-    <div className="flex items-center hover:bg-customCactus-300 hover:rounded-xl p-2 lg:p-3">
-      <span className="mr-2 lg:text">{icon}</span>
-      <a href='#' className=' lg:text-xl '>{text}</a>
+    <div className="flex items-center hover:bg-customCactus-300 hover:rounded-xl p-2">
+      <span className="mr-2 lg:text-lg">{icon}</span>
+      <a href='#' className="md:text-xl">{text}</a>
     </div>
   );
 }
