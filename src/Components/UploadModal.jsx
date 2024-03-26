0e239ee7694901332a5eb8c3c449e06159e36966
Karume-lab <danielkarume@outlook.com>
2024-03-26 19:52:26 +0300

    fix env vars

--- src/Components/UploadModal.jsx
+++ src/Components/UploadModal.jsx
@@ -26,8 +26,8 @@ const UploadModal = ({ toggleFileUploadModal, setToggleFileUploadModal, contract
                     {
                         headers: {
                             "Content-Type": "multipart/form-data",
-                            "pinata_api_key": process.env.VITE_PINATA_API_KEY,
-                            "pinata_secret_api_key": process.env.VITE_PINATA_SECRET_KEY,
+                            "pinata_api_key": process.env.REACT_APP_PINATA_API_KEY,
+                            "pinata_secret_api_key": process.env.REACT_APP_PINATA_SECRET_KEY,
                         },
                     }
                 );
