b510c8081ccd1633c183ea10806b0953e15ade65
Karume-lab <danielkarume@outlook.com>
2024-03-31 08:28:18 +0300

    fix bugs

---
+++ src/Components/FilesTable.jsx
@@ -0,0 +1,134 @@
+import React, { useMemo, useState } from 'react';
+import {
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import FileActions from "./FileActions"
+import d from "./dummyFiles.json";
+import { LuChevronFirst, LuChevronLast } from "react-icons/lu";
+import { MdNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
+import { IoSearch } from "react-icons/io5";
+
+const FilesTable = ({ files, columns, active }) => {
+    // files = d;
+    const data = useMemo(() => files, [files]);
+    const [sorting, setSorting] = useState([]);
+    const [filtering, setFiltering] = useState("");
+    const [activeFileId, setActiveFileId] = useState(0)
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            sorting: sorting,
+            globalFilter: filtering,
+        },
+        onSortingChange: setSorting,
+        onGlobalFilterChange: setFiltering,
+    });
+
+    return (
+        <div>
+            {(files && files.length) ?
+                <>
+                    <div className='flex items-center justify-end p-1 gap-1 flex-grow'>
+                        <IoSearch />
+                        <input
+                            className='h-7 rounded-lg  bg-customCactus-200 border-customCactus-400 focus:outline-none focus:ring-2 focus:ring-customCactus-400 w-96 sm:w-60 '
+                            type="text"
+                            placeholder='Search'
+                            value={filtering}
+                            onChange={(e) => setFiltering(e.target.value)}
+                        />
+                    </div>
+                    <table className='table-auto w-full max-h-full'>
+                        <thead className=''>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <tr key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => (
+                                        <th
+                                            key={header.id}
+                                            onClick={header.column.getToggleSortingHandler()}
+                                            className="px-4 py-2"
+                                        >
+                                            {header.isPlaceholder ? null : (
+                                                <div className="flex items-center">
+                                                    {flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                                    {
+                                                        { asc: "UP", desc: "DOWN" }[
+                                                        header.column.getIsSorted() ?? null
+                                                        ]
+                                                    }
+                                                </div>
+                                            )}
+                                        </th>
+                                    ))}
+                                    <th className="px-4 py-2"></th>
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody>
+                            {table.getRowModel().rows.map((row) => (
+                                <tr
+                                    key={row.id}
+                                    id={`${row.id}`}
+                                    className='border-b border-customCactus-400 hover:bg-customCactus-200 h-12 w-full'
+                                    onMouseOver={(e) => setActiveFileId(e.currentTarget.id)}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <td key={cell.id} className="px-4 py-2">
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </td>
+                                    ))}
+                                    {active === 2 ? "" :
+                                        <FileActions file={files[activeFileId]} />}
+                                </tr>
+                            ))}
+                        </tbody>
+
+
+                    </table>
+                    <div className='flex justify-center gap-2 mt-2'>
+                        <button onClick={() => table.setPageIndex(0)} className='bg-gray-400 rounded-sm hover:bg-customCactus-200'>
+                            <LuChevronFirst />
+                        </button>
+                        <button
+                            disabled={!table.getCanPreviousPage()}
+                            onClick={() => table.previousPage()}
+                            className='bg-gray-400 rounded-sm hover:bg-customCactus-200'
+                        >
+                            <GrFormPrevious />
+                        </button>
+                        <button
+                            disabled={!table.getCanNextPage()}
+                            onClick={() => table.nextPage()}
+                            className='bg-gray-400 rounded-sm hover:bg-customCactus-200'
+                        >
+                            <MdNavigateNext />
+                        </button>
+                        <button onClick={() => table.setPageIndex(table.getPageCount() - 1)} className='bg-gray-400 rounded-sm hover:bg-customCactus-200'>
+                            <LuChevronLast />
+                        </button>
+                    </div>
+                </>
+                :
+                <div>NO FILES UPLOADED YET ...</div>
+            }
+        </div>
+    )
+}
+
+export default FilesTable
+
+
--- src/Components/MyVault.jsx
+++ src/Components/MyVault.jsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import File from './File';
+import React, { useEffect } from "react";
 import { useSnackbar } from "notistack";
+import FilesTable from "./FilesTable";
 
 const MyVault = ({ files, setFiles, contract, account }) => {
     const { enqueueSnackbar } = useSnackbar();
-    const [otherAddress, setOtherAddress] = useState("");
+
+    const columns = [
+        { id: "name", header: "File", accessorKey: "name" },
+        {
+            id: "dateUploaded",
+            header: "Date Uploaded",
+            accessorKey: "dateUploaded",
+        },
+        { id: "tag", header: "Tag", accessorKey: "tag" },
+        { id: "size", header: "Size", accessorKey: "size" },
+    ];
+
     const getdata = async () => {
         let dataArray;
         try {
-            dataArray = await contract.getFiles(otherAddress || account);
+            dataArray = await contract.getFiles(account);
         } catch (error) {
-            enqueueSnackbar("You don't have access", { variant: 'error' });
+            enqueueSnackbar("You don't have access", { variant: "error" });
             console.error(error);
         }
         if (!dataArray) {
             setFiles(dataArray);
-            enqueueSnackbar('Fetched files successfully', { variant: 'success' });
+            enqueueSnackbar("Fetched files successfully", { variant: "success" });
         } else {
-            enqueueSnackbar('No file(s) to display', { variant: 'info' });
+            enqueueSnackbar("No file(s) to display", { variant: "info" });
         }
     };
+
     const timestamp2DateTime = (timestamp) => {
         let date;
         if (timestamp.toString() === "0") {
@@ -36,42 +48,13 @@ const MyVault = ({ files, setFiles, contract, account }) => {
 
     return (
         <div className="rounded-2xl bg-customCactus-100  h-full overflow-hidden flex flex-col p-2 text-customCactus-400">
-            <div className='flex items-center justify-center gap-4'>
-                <input
-                    type="text"
-                    placeholder="Enter Address"
-                    value={otherAddress}
-                    onChange={(e) => setOtherAddress(e.target.value)}
-                    className="address border rounded p-2 lg:w-96"
-                />
-                <button
-                    className="button bg-customCactus-400 hover:bg-customCactus-300 text-white font-bold py-2 px-4 rounded"
-                    onClick={getdata}
-                >
-                    Open Vault
-                </button>
-            </div>
             <div className="pt-1 pl-2 rounded-r-2xl-2xl rounded-t-2xl ">
-                <p className="ml-2 font-bold">My Vault</p>
+                <p className="ml-2 font-bold text-center text-2xl">MY VAULT</p>
             </div>
-            <div className='border-t border-1 w-11/12 self-center border-customCactus-400'></div>
-            <div className="flex flex-row justify-around text-xs pt-2">
-                <p>Name</p>
-                <p>Tag</p>
-                <p>Date </p>
-                <p>Size</p>
-            </div>
-            <div className="flex flex-col gap-2 overflow-auto h-80 p-3">
-                {files ? (
-                    files.map(({ owner, dateUploaded, dateModified, dateAccessed, isFavourite, isArchived, cid, name, description, extension, tag, size }, index) => (
-                        <File key={index} fileName={name} tag={tag} date={timestamp2DateTime(dateUploaded)} size={size} />
-                    ))
-                ) : (
-                    <div className="text-customCactus-400 text-5xl font-bold text-center mt-32">NO FILES UPLOADED YET ...</div>
-                )}
-            </div>
+            <div className="border-t border-1 w-full self-center border-customCactus-400 bg-red-700"></div>
+            <FilesTable files={files} columns={columns} />
         </div>
     );
-}
+};
 
-export default MyVault
+export default MyVault;
