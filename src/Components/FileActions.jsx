8167d7cc4388286b660f458d90d8f016a33c517d
Karume-lab <danielkarume@outlook.com>
2024-04-07 05:20:42 +0300

    modify favourite action depending on the file favourite state

--- src/Components/FileActions.jsx
+++ src/Components/FileActions.jsx
@@ -4,14 +4,12 @@ import { CiEdit } from "react-icons/ci";
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
-import { LuHeartOff } from "react-icons/lu";
 import fileDownload from 'js-file-download';
 import axios from "axios";
 
 
 const FileActions = ({ file, contract }) => {
     const [fileOptionsOpen, setFileOptionsOpen] = useState(false);
-    const [isFavorite, setIsFavorite] = useState(false); 
     const { cid, name, extension } = file;
 
     const handleDownload = () => {
@@ -29,11 +27,12 @@ const FileActions = ({ file, contract }) => {
     const handleMarkAsFavouriteClick = async () => {
         try {
             await contract.toggleFavourite(file.cid);
-            setIsFavorite(!isFavorite);
         } catch (error) {
             console.error(error);
         }
     }
+
+    // const 
 
     return (
         <td className="relative">
@@ -44,7 +43,7 @@ const FileActions = ({ file, contract }) => {
                 <div onMouseEnter={() => setFileOptionsOpen(true)} onMouseLeave={() => setFileOptionsOpen(false)} className="font-bold text-lg absolute right-0 z-10 mt-2 p-2 origin-top-right rounded-2xl bg-customCactus-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none flex flex-row justify-center gap-1">
                     <div title="Edit" data-tooltip-target="tooltip-top" data-tooltip-placement="top" className="p-1 rounded-full hover:bg-customCactus-100"><CiEdit /> </div>
                     <div title="Add to Favorites" className="p-1 rounded-full hover:bg-customCactus-100" onClick={() => handleMarkAsFavouriteClick()}>
-                        {isFavorite ? <LuHeartOff />: <MdFavoriteBorder />}
+                        {file.isFavourite ? <MdFavorite /> : <MdFavoriteBorder />}
                     </div>
                     <div title="Share" className="p-1 rounded-full hover:bg-customCactus-100"><IoShareSocialOutline /></div>
                     <div title="Download" className="p-1 rounded-full hover:bg-customCactus-100" onClick={() => handleDownload()}><MdOutlineFileDownload /> </div>
